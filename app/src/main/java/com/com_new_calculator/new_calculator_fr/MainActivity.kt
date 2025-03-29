baa1adf259195e4e3a27047d2caa960b73ee56a1
badjig turtoga <turtoga.leonardo11@gmail.com>
2025-03-30 04:42:02 +0800

    all Buttons apended

--- app/src/main/java/com/com_new_calculator/new_calculator_fr/MainActivity.kt
+++ app/src/main/java/com/com_new_calculator/new_calculator_fr/MainActivity.kt
@@ -12,6 +12,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var solution: TextView
     private lateinit var answer: TextView
+    var canAddDecimal = true
+    var canAddOperation = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,18 +27,39 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    fun numberButtons(view: View) {
+
+        if (view is Button) {
+            if (view.text == ".") {
+                if (canAddDecimal)
+                    solution.append(view.text)
+                    canAddDecimal = false
+            } else
+                solution.append(view.text)
+                canAddOperation = true
+        }
+    }
+//// from Deepseek
+//    fun numberButtons(view: View) {
+//        if (view is Button) {
+//            when {
+//                view.text == "." && canAddDecimal -> {
+//                    solution.append(view.text)
+//                    canAddDecimal = false
+//                }
+//                view.text != "." -> {
+//                    solution.append(view.text)
+//                    canAddOperation =true
+//                }
+//            }
+//        }
+//    }
 
     fun allClearButton(view: View) {
         solution.text = ""
         answer.text = ""
     }
 
-    fun numberButtons(view: View) {
-
-        if (view is Button) {
-            solution.append(view.text)
-        }
-    }
     fun backSpaceButton(view: View){
     var length = solution.length()
 
@@ -43,4 +67,20 @@ class MainActivity : AppCompatActivity() {
             solution.text = solution.text.subSequence(0,length - 1)
         }
     }
+
+    fun operationButtons(view: View) {
+        if(view is Button && canAddOperation){
+            solution.append(view.text)
+            canAddOperation = false
+            canAddDecimal = true
+        }
+    }
+    fun equalsButton(view: View) {
+
+
+
+    }
+
+
 }
+
